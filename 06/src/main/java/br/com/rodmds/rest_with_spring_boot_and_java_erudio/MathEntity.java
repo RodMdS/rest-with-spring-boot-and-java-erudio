61acabb36cc8fe30a92845328dafe057d0587a1b
Rod <rodrigomachadodossantos97@gmail.com>
2024-08-16 14:31:58 -0300

    Implementing simple calc operations system with spring boot

---
+++ 06/src/main/java/br/com/rodmds/rest_with_spring_boot_and_java_erudio/MathEntity.java
@@ -0,0 +1,29 @@
+package br.com.rodmds.rest_with_spring_boot_and_java_erudio;
+
+public class MathEntity {
+
+    public Double sum(Double numberOne, Double numberTwo) {
+        return numberOne + numberTwo;
+    }
+
+    public Double subtraction(Double numberOne, Double numberTwo) {
+        return numberOne - numberTwo;
+    }
+
+    public Double multiplication(Double numberOne, Double numberTwo) {
+        return numberOne * numberTwo;
+    }
+
+    public Double division(Double numberOne, Double numberTwo) {
+        return numberOne / numberTwo;
+    }
+
+    public Double mean(Double numberOne, Double numberTwo) {
+        return (numberOne + numberTwo) / 2;
+    }
+
+    public Double squareRoot(Double number) {
+        return Math.sqrt(number);
+    }
+
+}
